cac04c6f8fabb1dd21b70eac44b10502d9d32c5b
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2022-12-29 13:11:57 +0000

    17 routes2

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,21 @@ Rails.application.routes.draw do
   get 'about' => 'public/homes#about'
   namespace :admin do
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
     get '/' => 'homes#top'
   end
 
   scope module: :public do
     resources :items, only: [:index, :show]
-
+    resources :customers, only: [:show, :edit, :update]
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    resources :orders, only: [:new, :create, :index, :show]
+    get 'orders/comfirm' => 'orders#comfirm'
+    get 'orders/complete' => 'orders#complete'
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   end
 
 
