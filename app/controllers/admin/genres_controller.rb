7c9dcc9e42c05631bbaab8e838477f63ea257ea3
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2023-01-05 12:15:03 +0000

    28 view admin

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -9,8 +9,8 @@ class Admin::GenresController < ApplicationController
 
   def create
     @genre = Genre.new(genre_params)
-    genre.save
-    redirect_to admin_genre_path
+    @genre.save
+    redirect_to admin_genres_path
   end
 
   def edit
@@ -19,13 +19,13 @@ class Admin::GenresController < ApplicationController
 
   def update
     @genre = Genre.find(params[:id])
-    genre.update(genre_params)
+    @genre.update(genre_params)
     redirect_to admin_genres_path
   end
 
   private
 
   def genre_params
-   params.require(:genre).permit(:name)
+   params.permit(:name)
   end
 end
