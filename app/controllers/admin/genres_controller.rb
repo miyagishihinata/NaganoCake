82a124409ce0d9cfdf3161137eee23f4d706ccec
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2023-01-12 02:58:21 +0000

    42 cart con

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,6 @@
 class Admin::GenresController < ApplicationController
   def index
     @genres = Genre.all
-    @genre = Genre.new
   end
 
   def new
--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,27 @@
 class Public::CartItemsController < ApplicationController
+  def index
+    @cart_items = CartItem.all
+  end
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.save
+    redirect_to admin_genres_path
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
+  end
+  
+  private
+  
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount, :customer_id)
+  end
 end
