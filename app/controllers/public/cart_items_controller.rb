890e3bb8bdc85476d673fc8d0b14179b5f2cb7d1
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2023-01-12 06:01:10 +0000

    43 cart con view

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,21 +6,30 @@ class Public::CartItemsController < ApplicationController
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.save
-    redirect_to admin_genres_path
-  end
-
-  def edit
-    @genre = Genre.find(params[:id])
+    redirect_to cart_items_path
   end
 
   def update
-    @genre = Genre.find(params[:id])
-    @genre.update(genre_params)
-    redirect_to admin_genres_path
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
-  
+
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items_path
+  end
+
+  def destroy_all
+    @cart_items = CartItem.all
+    CartItem.destroy
+    redirect_to cart_items_path
+  end
+
+
   private
-  
+
   def cart_item_params
       params.require(:cart_item).permit(:item_id, :amount, :customer_id)
   end
