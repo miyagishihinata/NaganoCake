77797959c55b4ae9287b0db0987ca265a4887a5f
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2023-01-14 11:41:20 +0000

    56 public view cart model cart item

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
     get 'customers/unsubscribe' => 'customers#unsubscribe'
     patch 'customers/withdraw' => 'customers#withdraw'
 
-
+    
     delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
     resources :cart_items, only: [:index, :update, :destroy, :create]
 
