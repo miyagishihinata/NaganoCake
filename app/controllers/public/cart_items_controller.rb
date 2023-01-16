613829bbda67459fb9500175d778b7cae9584976
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2023-01-16 05:18:01 +0000

    61 public view cart

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,8 +7,19 @@ class Public::CartItemsController < ApplicationController
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
-    @cart_item.save
-    redirect_to cart_items_path
+
+
+    if cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+			 cart_item.amount += params[:cart_item][:amount].to_i cart_item.save
+			 current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).save
+			 redirect_to cart_items_path
+
+		elsif @cart_item.save
+			    redirect_to cart_items_path
+
+		else render 'items/(item.id)'
+
+    end
   end
 
   def update
