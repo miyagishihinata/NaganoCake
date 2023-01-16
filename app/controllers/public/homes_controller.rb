4b95b1de7c4e2c330c6f0c550a69db613f0af413
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2023-01-16 06:03:22 +0000

    62 public view con  top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,13 @@
 class Public::HomesController < ApplicationController
   def top
+    @items = Item.limit(4)
   end
 
   def about
   end
+
+  def item_params
+    params.require(:home).permit(:name, :price, :is_active, :genre_id, :image)
+  end
+
 end
