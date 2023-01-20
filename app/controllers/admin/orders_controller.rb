cdc1c1948ee3ff92562c86fd0786a2fff57d9e96
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2023-01-20 13:44:44 +0000

    78 admin order show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,9 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @total= 0
+    @order.order_details.each do |order_detail|
+      @total = @total + order_detail.amount*order_detail.price
+    end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,10 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+  
   has_many :cart_items, dependent: :destroy
   has_many :order_details
   belongs_to :genre
