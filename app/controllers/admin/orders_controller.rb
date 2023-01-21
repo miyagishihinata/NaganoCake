6338cd42a2d69e506786994608e7f7be7abe6d45
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2023-01-21 05:38:28 +0000

    79 admin order show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,8 +2,9 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @total= 0
+    @subtotal = 0
     @order.order_details.each do |order_detail|
-      @total = @total + order_detail.amount*order_detail.price
+      @total = @total + order_detail.amount*order_detail.with_tax_price
     end
   end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,4 +3,8 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
+def with_tax_price
+    (price * 1.1).floor
+end
+
 end
