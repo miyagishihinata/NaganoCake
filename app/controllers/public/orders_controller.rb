f289afbafa9d0ca458425342646b3d2a76e10fd9
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2023-01-24 04:47:37 +0000

    83 admin order order detail con

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -56,9 +56,10 @@ class Public::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @cart_items = current_customer.cart_items
     @total = 0
     @order.order_details.each do |order_detail|
-      @total = @total + order_detail.amount*order_detail.price
+      @total = @total + order_detail.amount*order_detail.with_tax_price
     end
   end
 
